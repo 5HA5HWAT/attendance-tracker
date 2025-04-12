923e72315c4cc43327183d81539ddc23dad774fe
kartikey-singh-k <144022665+kartikey-singh-k@users.noreply.github.com>
2025-04-12 18:34:35 +0530

    sign_in_up_page

--- frontend/public/new_signin_signup/script.js
+++ frontend/public/new_signin_signup/script.js
@@ -1,18 +1,18 @@
-function toggleForms(form) {
+function toggleForms(type) {
     const signInForm = document.getElementById('signInForm');
     const signUpForm = document.getElementById('signUpForm');
     const signInBtn = document.getElementById('signInBtn');
     const signUpBtn = document.getElementById('signUpBtn');
 
-    if (form === 'signIn') {
-        signInForm.classList.remove('d-none');
-        signUpForm.classList.add('d-none');
-        signInBtn.classList.add('active');
-        signUpBtn.classList.remove('active');
+    if (type === 'signIn') {
+      signInForm.classList.remove('d-none');
+      signUpForm.classList.add('d-none');
+      signInBtn.classList.add('active');
+      signUpBtn.classList.remove('active');
     } else {
-        signUpForm.classList.remove('d-none');
-        signInForm.classList.add('d-none');
-        signUpBtn.classList.add('active');
-        signInBtn.classList.remove('active');
+      signUpForm.classList.remove('d-none');
+      signInForm.classList.add('d-none');
+      signUpBtn.classList.add('active');
+      signInBtn.classList.remove('active');
     }
-}
+  }
